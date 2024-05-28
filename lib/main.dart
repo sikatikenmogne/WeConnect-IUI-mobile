0fbb71c1acca27ff548158d6e42960e5ea65e47f
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-05-28 18:54:11 +0100

    chore: Update Supabase API credentials and initialize Supabase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:we_connect_iui_mobile/firebase_options.dart';
 
 import 'src/app.dart';
@@ -17,6 +18,11 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
