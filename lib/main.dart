fd25b81d1bc7fd7c7a5ceb60a1abcf970f3acc5a
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-02 17:36:07 +0100

    chore: update Supabase connection initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,10 @@ void main() async {
 
   // ! Comment the following line to disable the Supabase connection initialize
   await Supabase.initialize(
-    url: dotenv.env[
-        'SUPABASE_URL']!, // Fetch the Supabase URL from the environment variables
-    anonKey: dotenv.env[
-        'SUPABASE_ANON_KEY']!, // Fetch the Supabase public anon key from the environment variables
+    url:
+        'https://yojnxscjecnlltwblvrn.supabase.co', // Fetch the Supabase URL from the environment variables
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlvam54c2NqZWNubGx0d2JsdnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODg1NDUsImV4cCI6MjAzMjQ2NDU0NX0.zH1CZLJPZ-HLO768Vis_8VFgDMmKbpkglVBCYHUcgP0', // Fetch the Supabase public anon key from the environment variables
   );
 
   // Set up the SettingsController, which will glue user settings to multiple
