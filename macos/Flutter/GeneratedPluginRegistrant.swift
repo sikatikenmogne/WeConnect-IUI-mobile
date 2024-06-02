772ade9c385da80c2a8ee0f93be46c04e9cdd6d5
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-02 08:50:01 +0100

    chore: remove unused Firebase plugins and update dependencies
    Remove unused Firebase plugins `firebase_auth` and `firebase_core` from the `GeneratedPluginRegistrant.swift` file. This improves the build time and reduces unnecessary dependencies. Also, update the `pubspec.yaml` and `pubspec.lock` files to reflect the removal of these plugins and update other dependencies to their latest versions.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import firebase_auth
-import firebase_core
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -15,8 +13,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
