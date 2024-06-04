96a5ee441d6e57925dd6f05bbf3628bcc7515a35
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-04 15:55:21 +0100

    chore: remove supabase plugin and update loginPage.dart to use supabase_flutter package

--- lib/src/controller/login_controller.dart
+++ lib/src/controller/login_controller.dart
@@ -1,4 +1,4 @@
-import 'package:supabase/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController {
