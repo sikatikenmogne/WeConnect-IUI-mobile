c9afa8d6bf9fee6f81009d7869d5809d1186db45
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-07 09:58:39 +0100

    refactor: update theme mode persistence in SettingsService
    The code changes in settings_service.dart refactor the themeMode method and updateThemeMode method to use shared_preferences package for persisting the user's preferred ThemeMode. This improves the maintainability and flexibility of the SettingsService class by separating the storage logic from the service logic.

--- lib/src/service/settings/settings_service.dart
+++ lib/src/service/settings/settings_service.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-/// A service that stores and retrieves user settings.
-///
-/// By default, this class does not persist user settings. If you'd like to
-/// persist the user settings locally, use the shared_preferences package. If
-/// you'd like to store settings on a web server, use the http package.
 class SettingsService {
-  /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  /// Persists the user's preferred ThemeMode to local or remote storage.
+  Future<ThemeMode> themeMode() async {
+    final SharedPreferences prefs = await _prefs;
+    return (prefs.getString('themeMode') ?? 'system') == 'light'
+        ? ThemeMode.light
+        : (prefs.getString('themeMode') == 'dark' ? ThemeMode.dark : ThemeMode.system);
+  }
+
   Future<void> updateThemeMode(ThemeMode theme) async {
-    // Use the shared_preferences package to persist settings locally or the
-    // http package to persist settings over the network.
+    final SharedPreferences prefs = await _prefs;
+    String themeString;
+    switch (theme) {
+      case ThemeMode.light:
+        themeString = 'light';
+        break;
+      case ThemeMode.dark:
+        themeString = 'dark';
+        break;
+      default:
+        themeString = 'system';
+    }
+    await prefs.setString('themeMode', themeString);
   }
 }
